d4acf5edbed16e21506c814dd2c472cb83a2e2c3
itc-s24019 <s24019@std.it-college.ac.jp>
2024-07-04 07:58:42 +0000

    2024/07/04

---
+++ 2-6.py
@@ -0,0 +1,19 @@
+a = 'abc'
+b = 'xyz'
+print("{} {}".format(a, b))
+
+title = 'name'
+value = 'John Smith'
+print( 'Your {} is {}.'.format(title, value))
+
+age = 20
+print('Your age is {}.'.format(age))
+
+title = 'IQ'
+value = 100
+print('{} = {}'.format(title, value))
+
+#Python3.6以降で使える「フォーマット済み文字列リテラル」
+a, b = "Hello", "World"
+print(f"{a}{b}")
+
---
+++ 2-9.py
@@ -0,0 +1,9 @@
+my_list =[0, 2, 4, 6, 8, 10]
+print(2 in my_list)
+
+my_list =[0, 2, 4, 6, 8, 10]
+print(3 in my_list)
+
+my_list =[0, 2, 4, 6, 8, 10]
+print(3 not in my_list)
+
