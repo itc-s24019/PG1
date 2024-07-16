2198d400333ca0c8049f9782ee7520f9d9ab4d41
itc-s24019 <s24019@std.it-college.ac.jp>
2024-07-16 08:00:15 +0000

    2024/07/16

---
+++ 4-3.py
@@ -0,0 +1,10 @@
+def show_args(*args):
+    '''与えられた複数の位置引数をタプルにまとめて受け取りそのタプルを表示して返す'''
+    print('Posotional arguments:, args')
+    return args
+
+def show_kwargs(**kwargs):
+   ''' 与えられた複数の位置引数をタプルにまとめて受け取りそのタプルを表示して返す'''
+   print('keyword arguments:', kwargs)
+   return kwargs
+
